8f1803b1c3fd04a54903aa323730ba54453f8a0b
Sidharth Sudarsan <git@sidharth.me>
2022-04-04 20:47:28 -0400

    add auth middleware

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import path from 'path';
 import makeExpressCallback from './express';
 import productController from './controllers';
 import http from 'http';
+import auth from './middleware/auth';
 
 dotenv.config();
 
@@ -16,14 +17,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res, next) {
-	res.send('Unicorn Store Identity Microservice REST API');
+app.get('/', auth, function (req, res, next) {
+	res.send('Unicorn Store Products Microservice REST API');
 });
-app.post(`/${apiRoot}/add`, makeExpressCallback(productController.postProduct));
-app.get(`/${apiRoot}/getAll`, makeExpressCallback(productController.getAllProducts));
+app.post(`/${apiRoot}/add`, auth, makeExpressCallback(productController.postProduct));
+app.get(`/${apiRoot}/getAll`, auth, makeExpressCallback(productController.getAllProducts));
 
 // Get port from environment and store in Express.
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 
 // Create HTTP server.
