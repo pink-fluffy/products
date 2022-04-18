bbd44a9bdf2c1a62e554ed4769ce68a8ebf6f510
Sidharth Sudarsan <git@sidharth.me>
2022-04-18 01:33:31 -0400

    add user's name to review

--- src/use-cases/add-review.js
+++ src/use-cases/add-review.js
@@ -1,5 +1,7 @@
 import enums from '../enums';
 import makeReview from '../review';
+import axios from 'axios';
+const config = process.env;
 /**
  *
  * @param {*} productsDb
@@ -17,10 +19,31 @@ export default function makeAddReview(productsDb) {
 			throw { status: enums.STATUS_CODES.NOT_FOUND, message: enums.REASON_PHRASES.NOT_FOUND };
 		}
 		const review = makeReview(reviewInfo);
-		return productsDb.addReview(product_id, {
+		var reviewedProduct = await productsDb.addReview(product_id, {
 			userId: review.getUserId(),
 			rating: review.getRating(),
 			text: review.getText()
 		});
+
+		for (var i = 0; i < reviewedProduct.reviews.length; i++) {
+			var productReview = reviewedProduct.reviews[i];
+			const userId = productReview.userId;
+			await axios({
+				method: 'GET',
+				url: `http://${config.IDENTITY_HOST}:${config.IDENTITY_PORT}/user/${userId}`
+			})
+				.then(function (res) {
+					const name = `${res.data.data.first_name} ${res.data.data.last_name}`;
+
+					productReview.userId = name;
+					reviewedProduct.reviews[i] = productReview;
+					console.log(reviewedProduct.reviews[i].userId);
+				})
+				.catch(function (err) {
+					throw err;
+				});
+		}
+
+		return reviewedProduct;
 	};
 }
--- src/use-cases/get-product.js
+++ src/use-cases/get-product.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+const config = process.env;
 /**
  *
  * @param {*} productsDb
@@ -5,6 +7,25 @@
  */
 export default function makeGetProduct(productsDb) {
 	return async function getProduct(id) {
-		return productsDb.findByShortId(id);
+		var product = await productsDb.findByShortId(id);
+		for (var i = 0; i < product.reviews.length; i++) {
+			var productReview = product.reviews[i];
+			const userId = productReview.userId;
+			await axios({
+				method: 'GET',
+				url: `http://${config.IDENTITY_HOST}:${config.IDENTITY_PORT}/user/${userId}`
+			})
+				.then(function (res) {
+					const name = `${res.data.data.first_name} ${res.data.data.last_name}`;
+
+					productReview.userId = name;
+					product.reviews[i] = productReview;
+					console.log(product.reviews[i].userId);
+				})
+				.catch(function (err) {
+					throw err;
+				});
+		}
+		return product;
 	};
 }
